c87d24ef9868b440242fd48368178028a204960f
AdityaRoot <adityavmangla@gmail.com>
2023-03-13 01:10:30 +0800

    Fixing macros

--- .config/nvim/lua/adi/lualine.lua
+++ .config/nvim/lua/adi/lualine.lua
@@ -1,14 +1,17 @@
-
 require('lualine').setup {
-  options = {
-    theme = 'kanagawa'
-  },
-   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'encoding', 'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
+    options = {
+        theme = 'kanagawa'
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { {
+            require("noice").api.statusline.mode.get,
+            cond = require("noice").api.statusline.mode.has,
+            color = { fg = "#ff9e64" },
+        }, 'encoding', 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
 }
