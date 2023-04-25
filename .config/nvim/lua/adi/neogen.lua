c85dc0e04e738c795f187ff2b6a65825742d0e51
AdityaRoot <adityavmangla@gmail.com>
2023-04-25 16:24:22 +0800

    Neogen config

---
+++ .config/nvim/lua/adi/neogen.lua
@@ -0,0 +1,5 @@
+require('neogen').setup {
+    enabled = true,             --if you want to disable Neogen
+    input_after_comment = true, -- (default: true) automatic jump (with insert mode) on inserted annotation
+    snippet_engine = "luasnip"
+}
